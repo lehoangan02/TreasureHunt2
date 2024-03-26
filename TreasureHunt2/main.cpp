42f0039fa34bb069b210ff13a4db9fffd06624eb
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-03-26 12:22:50 +0700

    Initial Commit

---
+++ TreasureHunt2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TreasureHunt2
+//
+//  Created by Le Hoang An on 26/3/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
